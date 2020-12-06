5a4ad30361362e8e006237f5e13774816f218637
hsiblini <70350020+hsiblini@users.noreply.github.com>
2020-12-06 18:34:44 -0500

    Add files via upload

--- MechaCar_Challenge.R
+++ MechaCar_Challenge.R
@@ -1,3 +1,4 @@
+#DELIVERABLE1
 library(tidyverse)
 library(dplyr)
 df <- read.csv('MechaCar_mpg.csv', check.names = F, stringsAsFactors = F)
@@ -5,3 +6,22 @@ head(df)
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = df)
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = df))
 
+#DELIVERABLE2
+table_sc <- read.csv('Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
+head(table_sc)
+total_summary <- table_sc %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+lot_summary <- table_sc %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups='keep')
+
+#DELIVERABLE3
+?t.test() 
+head(table_sc)
+t.test(table_sc$PSI, mu = 1499)
+
+lot1 <- subset(table_sc, Manufacturing_Lot=='Lot1')
+t.test(lot1$PSI, mu = 1499)
+
+lot2 <- subset(table_sc, Manufacturing_Lot=='Lot2')
+t.test(lot2$PSI, mu = 1499)
+
+lot3 <- subset(table_sc, Manufacturing_Lot=='Lot3')
+t.test(lot3$PSI, mu = 1499)
